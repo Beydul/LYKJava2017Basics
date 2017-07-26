8aef96772795697a23750e42b8c87bc1bf3567b2
Ceyhun Onur <ceyhun.onur@ozguryazilim.com.tr>
2017-07-26 11:50:29 +0300

    fixed package names

--- src/tr/org/linux/kamp/caeasercipher/CaesarCipher.java
+++ src/tr/org/linux/kamp/caeasercipher/CaesarCipher.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package caeasercipher;
+package tr.org.linux.kamp.caeasercipher;
 
 import java.util.Scanner;
 
